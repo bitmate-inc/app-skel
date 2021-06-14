a3b8ffbcf273e3b0c051f14f7e829ffd79cf69d2
Miroslav Braun <braun.miroslav@gmail.com>
2021-06-14 23:36:23 +0200

    fix: missing module import

--- nodejs-backend/src/app/cli/cli.module.ts
+++ nodejs-backend/src/app/cli/cli.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CoreModule } from '../../core/core.module';
+import { CommandModule } from 'nestjs-command';
 
 @Module({
 	imports: [
+		CommandModule,
 		CoreModule,
 	],
 	providers: [],
